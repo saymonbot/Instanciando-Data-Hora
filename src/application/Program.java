6ac53ee538de39f70c36c8cd53dac641d602551d
saymonbot <saymon_botelho123@outlook.com>
2024-03-06 21:28:39 -0300

    Coverting date-hour global to local

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,11 @@ public class Program {
 		LocalDateTime d05 = LocalDateTime.parse("2024-03-06T18:13:05");
 		Instant d06 = Instant.parse("2024-03-06T18:13:05Z");
 		
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		
 		Instant d07 = Instant.parse("2024-03-06T18:13:05-03:00");
 		
 		LocalDate d08 = LocalDate.parse("06/03/2024", fmt1);
@@ -37,13 +42,22 @@ public class Program {
 		System.out.println("d03 = " + d03);
 		System.out.println("d04 = " + d04);
 		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mes = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
 		System.out.println("d05 = " + d05);
 		System.out.println("d05 = " + d05.format(fmt1));
 		System.out.println("d05 = " + d05.format(fmt2));
 		System.out.println("d05 = " + d05.format(fmt4));
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 minuto = " + d05.getMinute());
 		System.out.println("d06 = " + d06); 
 		System.out.println("d06 = " + fmt3.format(d06)); 
 		System.out.println("d06 = " + fmt5.format(d06));
+		System.out.println("r1 = " + r1); 
+		System.out.println("r2 = " + r2); 
+		System.out.println("r3 = " + r3); 
+		System.out.println("r4 = " + r4); 
 		System.out.println("d07 = " + d07); 
 		System.out.println("d08 = " + d08); 
 		System.out.println("d09 = " + d09); 
